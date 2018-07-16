d9c5f514809ad54dba93daf9dc85cb225bb99f59
Unknown <228089@student.pwr.edu.pl>
2018-07-16 23:28:18 +0200

    initial commit

---
+++ java-spring-quick-start/src/main/java/com/example/javaspringquickstart/JavaSpringQuickStartApplication.java
@@ -0,0 +1,12 @@
+package com.example.javaspringquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringQuickStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringQuickStartApplication.class, args);
+	}
+}
