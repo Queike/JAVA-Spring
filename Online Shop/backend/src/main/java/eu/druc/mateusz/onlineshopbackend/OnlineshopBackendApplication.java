37ddd5fdebb8c5507bffef7275e3004b36ce0b07
Unknown <228089@student.pwr.edu.pl>
2018-07-18 21:51:06 +0200

    initial commit

---
+++ Online Shop/backend/src/main/java/eu/druc/mateusz/onlineshopbackend/OnlineshopBackendApplication.java
@@ -0,0 +1,12 @@
+package eu.druc.mateusz.onlineshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopBackendApplication.class, args);
+	}
+}
---
+++ Online Shop/backend/src/test/java/eu/druc/mateusz/onlineshopbackend/OnlineshopBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.druc.mateusz.onlineshopbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineshopBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
